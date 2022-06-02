313184e10bc6fdc468b088d6f34a3ec32b3d8011
Maqel <98158452+Maqel@users.noreply.github.com>
2022-06-02 20:56:53 +0200

    select_table_Sending_data_fixed

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -32,7 +32,6 @@ class Booking{
         endDateParam,
       ],
     };
-    //console.log('getData params', params);
 
     const urls = {
       booking:       settings.db.url + '/' + settings.db.bookings + '?' + params.booking.join('&'),
@@ -66,56 +65,54 @@ class Booking{
   }
   parseData(booking, eventsCurrent, eventsRepeat) {
     const thisBooking = this;
-
     thisBooking.booked = {};
-
+    /*START LOOP*/
     for (let item of booking) {
       thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
-    }
+    }/*END LOOP*/ /*START LOOP*/
     for (let item of eventsCurrent) {
       thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
-    }
+    }/*END LOOP*/ /*START LOOP*/
     const minDate = thisBooking.datePicker.minDate;
     const maxDate = thisBooking.datePicker.maxDate;
-
+    /*START LOOP*/
     for (let item of eventsRepeat) {
       if (item.repeat == 'daily') {
-
+        /*START LOOP*/
         for (let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)) {
           thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
-        }
+        }/*END LOOP*/
       }
-    }
+    }/*END LOOP*/
     //console.log('thisBooking.booked', thisBooking.booked);
     thisBooking.updateDOM();
   }
   makeBooked(date, hour, duration, table){
     const thisBooking = this;
-
     if (typeof thisBooking.booked[date] == 'undefined') {
       thisBooking.booked[date] = {};
     }
     const startHour = utils.hourToNumber(hour);
-
+    /*START LOOP*/
     for (let hourBlock = startHour; hourBlock < startHour + duration; hourBlock += 0.5) {
       if (typeof thisBooking.booked[date][hourBlock] == 'undefined') {
         thisBooking.booked[date][hourBlock] = [];
       }
       thisBooking.booked[date][hourBlock].push(table);
-    }
+    }/*END LOOP*/
   }
   updateDOM() {
     const thisBooking = this;
     thisBooking.date = thisBooking.datePicker.value;
     thisBooking.hour = utils.hourToNumber(thisBooking.hourPicker.value);
     let allAvailable = false;
-
     if (typeof thisBooking.booked[thisBooking.date] == 'undefined'
       ||
       typeof thisBooking.booked[thisBooking.date][thisBooking.hour] == 'undefined'
     ) {
       allAvailable = true;
     }
+    /*START LOOP*/
     for (let table of thisBooking.dom.tables) {
       let tableId = table.getAttribute(settings.booking.tableIdAttribute);
       if (!isNaN(tableId)) {
@@ -130,7 +127,7 @@ class Booking{
         table.classList.remove(classNames.booking.tableBooked);
         table.classList.remove(classNames.booking.tableSelected);
       }
-    }
+    }/*END LOOP*/
   }
 
   render(element){
@@ -183,14 +180,14 @@ class Booking{
     console.log(event.target);
     const targetTableId = targetTable.getAttribute('data-table');
     console.log(targetTableId);
-
     if (
       !targetTable.classList.contains(classNames.booking.tableBooked) &&
       !targetTable.classList.contains(classNames.booking.tableSelected)
     ) {
+      /*START LOOP*/
       for (let table of thisBooking.dom.tables) {
         table.classList.remove(classNames.booking.tableSelected);
-      }
+      }/*END LOOP*/
       targetTable.classList.add(classNames.booking.tableSelected);
       thisBooking.tableSelectedData = targetTableId;
       console.log(thisBooking.tableSelectedData);
@@ -206,9 +203,6 @@ class Booking{
 
   choseStarters(event) {
     const thisBooking = this;
-
-    // e.preventDefault();
-    // console.log(e.target);
     const clickedChecbox = event.target;
     if (clickedChecbox.type === 'checkbox' && clickedChecbox.name === 'starter')
       if (clickedChecbox.checked) {
@@ -219,7 +213,6 @@ class Booking{
         const bookingStarterIndex = thisBooking.dom.starters.indexOf(
           event.target.value
         );
-
         thisBooking.dom.starters.splice(bookingStarterIndex, 1);
         console.log(thisBooking.dom.starters);
       }
@@ -259,7 +252,6 @@ class Booking{
       })
       .then(function (parsedResponse) {
         console.log('parsedResponse', parsedResponse);
-
         thisBooking.booked = parsedResponse;
         console.log(thisBooking.booked);
       });
